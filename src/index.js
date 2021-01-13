0d232ca02a3264b88db68c65f60060f0d1653f87
march-75 <55731816+march-75@users.noreply.github.com>
2021-01-13 16:16:43 +0900

    13日の

---
+++ src/index.js
@@ -0,0 +1,27 @@
+/**
+ * スプレット構文
+ */
+
+// const arr1 = [1, 2, 3, 4, 5, 6];
+
+// console.log(arr1);
+// console.log(...arr1);
+
+// const sumFunc = (num1, num2, num3, num4) =>
+//   console.log(`合計は${num1 + num2 + num3}`);
+// // sumFunc(arr1[0], arr1[1]);
+// sumFunc(...arr1);
+
+//まとめる場合
+
+const arr2 = [1, 2, 3, 4, 5];
+const [num1, num2, ...arr3] = arr2;
+console.log(num1);
+console.log(num2);
+console.log(arr3);
+
+const arr4 = [10, 20, 30, 40, 50];
+const arr6 = [...arr2];
+console.log(arr6);
+const arr7 = [...arr2, ...arr4];
+console.log(arr7);
