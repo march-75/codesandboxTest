ac75eb7f137ac91014d439b2f4997de15bb7e6b2
march-75 <55731816+march-75@users.noreply.github.com>
2021-01-13 16:41:08 +0900

    ぷんじろうとか

--- src/index.js
+++ src/index.js
@@ -14,14 +14,29 @@
 
 //まとめる場合
 
-const arr2 = [1, 2, 3, 4, 5];
-const [num1, num2, ...arr3] = arr2;
-console.log(num1);
-console.log(num2);
-console.log(arr3);
+// const arr2 = [1, 2, 3, 4, 5];
+// const [num1, num2, ...arr3] = arr2;
+// console.log(num1);
+// console.log(num2);
+// console.log(arr3);
 
-const arr4 = [10, 20, 30, 40, 50];
-const arr6 = [...arr2];
-console.log(arr6);
-const arr7 = [...arr2, ...arr4];
-console.log(arr7);
+// const arr4 = [10, 20, 30, 40, 50];
+// const arr6 = [...arr2];
+// console.log(arr6);
+// const arr7 = [...arr2, ...arr4];
+// console.log(arr7);
+
+/**
+ * mapやfilterを使った処理
+ */
+
+const nameArr = ["田中", "山田", "ごんたろう", "げんごろう", "ぷんじろう"];
+for (let index = 0; index < nameArr.length; index++) {
+  console.log(nameArr[index]);
+}
+
+const nameArr2 = nameArr.map((name) => {
+  console.log(`順番に,${name}`);
+  return name;
+});
+console.log(nameArr2);
